0bd87df4da41520588487c287b5d25cd7913afbd
Mohamed Samemullah <94099498+MdSamemullah@users.noreply.github.com>
2023-10-25 01:36:30 +0530

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+function clr(){
+    document.getElementById('backclr').style.background = "#dab7da";
+}   
+function text(){
+    document.getElementById('function').innerHTML = " Sometimes a man has to make hard choice, choice that might look wrong to others.";
+}
+function myFunction(){
+    clr();
+    text();
+}
